6578be8e298053552c709d2a2538454226eb5e9a
loralridz <noorrida2000@gmail.com>
2021-04-13 09:58:31 -0700

    game layout

--- App.js
+++ App.js
@@ -1,19 +1,19 @@
-import React, { useState } from 'react';
-import { Pressable, StyleSheet, Text, View } from 'react-native';
-
+import React, { useState } from "react";
+import { Pressable, StyleSheet, Text, View, Button } from "react-native";
+import "./assets/App.css";
 const App = () => {
   const [timesPressed, setTimesPressed] = useState(0);
 
-  let textLog = '';
+  let textLog = "";
   if (timesPressed > 1) {
-    textLog = timesPressed + 'x onPress';
+    textLog = timesPressed + "x onPress";
   } else if (timesPressed > 0) {
-    textLog = 'onPress';
+    textLog = "onPress";
   }
 
   return (
     <View style={styles.container}>
-      <Pressable
+      {/* <Pressable
         onPress={() => {
           setTimesPressed((current) => current + 1);
         }}
@@ -32,8 +32,52 @@ const App = () => {
         )}
       </Pressable>
       <View style={styles.logBox}>
-        <Text testID="pressable_press_console">{textLog}</Text>
+        <Text testID="pressable_press_console">{textLog}</Text>    </View> */}
+      {/* <h1 style={{color:"salmon"}}>Number Guessing Game</h1> */}
+      {/* <Header fullname="Noor Rida" /> */}
+      {/* <Text style={{ fontSize: 50 }}>Number: {getText}</Text> */}
+      {/* <View style={{ flexDirection: "row", padding: "3rem" }}> */}
+      {/* <Button title="Clear"  onPress={()=>setText(0)} />
+         <Button title="Reset" onPress={()=>setText("")} /> */}
+      {/* </View> */}
+      <Text style={styles.playerText}>Player 1 : X</Text>
+      <View style={styles.board}>
+        <View style={styles.row}>
+          <View style={styles.button}>
+            <button title="" />
+          </View>
+          <View style={styles.button}>
+            <button title="" />
+          </View>
+          <View style={styles.button}>
+            <button title="" />
+          </View>
+        </View>
+        <View style={styles.row}>
+          <View style={styles.button}>
+            <button title="" />
+          </View>
+          <View style={styles.button}>
+            <button title="" />
+          </View>
+          <View style={styles.button}>
+            <button title="" />
+          </View>
+        </View>
+        <View style={styles.row}>
+          <View style={styles.button}>
+            <button title="" />
+          </View>
+          <View style={styles.button}>
+            <button title="" />
+          </View>
+          <View style={styles.button}>
+            <button title="" />
+          </View>
+        </View>
       </View>
+
+      <Text style={styles.playerText}>Player 2 : O</Text>
     </View>
   );
 };
@@ -41,21 +85,29 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
   },
-  text: {
-    fontSize: 16
+  row: {
+    flexDirection: "row"
   },
-  wrapperCustom: {
-    borderRadius: 8,
-    padding: 6
+  board: {
+    width: 600,
+    height: 600,
+    overflow: "hidden"
   },
-  logBox: {
-    padding: 20,
-    margin: 10,
-    borderWidth: StyleSheet.hairlineWidth,
-    borderColor: '#f0f0f0',
-    backgroundColor: '#f9f9f9'
+  button: {
+    backgroundColor: "grey",
+    borderWidth: 3,
+    borderColor: "#000",
+    width: 200,
+    height: 200
+  },
+  playerText: {
+    padding: "3rem",
+    fontSize: "26px",
+    fontFamily: "monospace"
   }
 });
 
